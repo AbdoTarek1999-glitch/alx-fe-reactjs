9e6aa96635c0ce371657d7e34a6f514d420cf793
AbdoTarek1999-glitch <abdotarek99@gmail.com>
2025-11-09 23:58:05 +0200

    Refactor to use Context API and add Counter component with inline CSS

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Header from './components/Header';
-import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import Counter from './components/Counter';
+import UserProfile from './components/UserProfile';
+import UserContext from './UserContext'; // استدعاء الContext
 
 function App() {
+  const userData = { name: "Abdo", age: 25, bio: "React Developer" };
+
   return (
-    <div>
-      <Header />
-      <UserProfile name="Abdo" age={25} bio="React Developer" />
-      <MainContent />
-      <Counter />
-      <Footer />
-    </div>
+    <UserContext.Provider value={userData}>
+      <div>
+        <Header />
+        <UserProfile /> {/* لم نعد نمرر props */}
+        <MainContent />
+        <Counter />
+        <Footer />
+      </div>
+    </UserContext.Provider>
   );
 }
 
