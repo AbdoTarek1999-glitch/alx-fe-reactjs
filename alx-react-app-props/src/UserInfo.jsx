f60e9533d9add9f1a9450e790e162304d06ee56d
AbdoTarek1999-glitch <62442030+AbdoTarek1999-glitch@users.noreply.github.com>
2025-11-13 10:54:10 +0200

    Create UserInfo.jsx

---
+++ alx-react-app-props/src/UserInfo.jsx
@@ -0,0 +1,9 @@
+// src/UserInfo.jsx
+import React from 'react'; // (ملاحظة: استيراد React هنا غير ضروري ولكنه غير ضار)
+import UserDetails from './UserDetails';
+
+function UserInfo() {
+  return <UserDetails />;
+}
+
+export default UserInfo;
