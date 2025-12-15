652436a5971172df076fd588fa5ddad1214c3725
AbdoTarek1999-glitch <62442030+AbdoTarek1999-glitch@users.noreply.github.com>
2025-12-15 06:41:30 +0200

    Update BlogPost.jsx

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const BlogPost = () => {
-  // استخدام useParams لسحب الـ ID
+  // سحب الـ ID
   const { id } = useParams();
   
   return (
     <div style={{ padding: '20px', border: '1px solid #ff4500', margin: '20px' }}>
-      <h2>تفاصيل مقالة المدونة (مسار ديناميكي)</h2>
+      <h2>تفاصيل مقالة المدونة (مسار /blog/:id)</h2>
       <p>أنت تشاهد المقالة رقم: <strong>{id}</strong></p>
-      <p>هنا يتم جلب وعرض بيانات المقالة بالكامل...</p>
     </div>
   );
 };
